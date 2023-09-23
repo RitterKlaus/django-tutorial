ff0fb7a279a5592da116da58f71365c3cafa6477
RitterKlaus <48477737+RitterKlaus@users.noreply.github.com>
2023-09-23 18:03:56 +0200

    Anpassungen für lokal und static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ojs544fak-%d)pib13mjl%0_!7=mm03$p$o27yc_dnd1$0z=(#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ritterklaus.eu.pythonanywhere.com']
+ALLOWED_HOSTS = ['ritterklaus.eu.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/RitterKlaus/mysite/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
